7213da191fb2f4c97df015f13419a7b0d0bc6b05
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2024-12-25 07:06:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
     window.OneSignalDeferred = window.OneSignalDeferred || [];
     OneSignalDeferred.push(async function (OneSignal) {
